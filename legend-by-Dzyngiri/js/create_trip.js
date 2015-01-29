d165f93125081f0245d953721c4003ea4489de59
Qiankun Zhuang <zhuang1992@gmail.com>
2015-01-29 00:38:23 -0500

    create_trip

---
+++ legend-by-Dzyngiri/js/create_trip.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+
+    var selectedClass = 'ui-state-highlight',
+        clickDelay = 600,
+        // click time (milliseconds)
+        lastClick, diffClick; // timestamps
+
+    $("#draggable li")
+    // Script to deferentiate a click from a mousedown for drag event
+    .bind('mousedown mouseup', function(e) {
+        if (e.type == "mousedown") {
+            lastClick = e.timeStamp; // get mousedown time
+        } else {
+            diffClick = e.timeStamp - lastClick;
+            if (diffClick < clickDelay) {
+                // add selected class to group draggable objects
+                $(this).toggleClass(selectedClass);
+            }
+        }
+    })
+    .draggable({
+        revertDuration: 10,
+        // grouped items animate separately, so leave this number low
+        containment: '.demo',
+        start: function(e, ui) {
+            ui.helper.addClass(selectedClass);
+        },
+        stop: function(e, ui) {
+            // reset group positions
+            $('.' + selectedClass).css({
+                top: 0,
+                left: 0
+            });
+        },
+        drag: function(e, ui) {
+            // set selected group position to main dragged object
+            // this works because the position is relative to the starting position
+            $('.' + selectedClass).css({
+                top: ui.position.top,
+                left: ui.position.left
+            });
+        }
+    });
+
+    $("#droppable, #draggable").sortable().droppable({
+        drop: function(e, ui) {
+            $('.' + selectedClass).appendTo($(this)).add(ui.draggable) // ui.draggable is appended by the script, so add it after
+            .removeClass(selectedClass).css({
+                top: 0,
+                left: 0
+            });
+        }
+    });
+
+});
