ea7627b15d9934dc24d9c3b11b5b7d74c4f2a096
yanchenl <yanchenl@andrew.cmu.edu>
2015-03-18 13:52:27 -0400

    add friend_id

--- travelgene/MongoDB/createDB.js
+++ travelgene/MongoDB/createDB.js
@@ -1,19 +1,19 @@
 use travelgene
-document=({"user_id":"00000001", "user_name":"liuyancheng", "password":"123456", "email":"liuyancheng@gmail.com", "phone":"4125204000", "birth":new Date('May 16, 1991'),"trip_id":["0001","0002"]});
+document=({"user_id":"00000001", "user_name":"liuyancheng", "password":"123456", "email":"liuyancheng@gmail.com", "phone":"4125204000", "birth":new Date('May 16, 1991'),"trip_id":["0001","0002"],"friend_id":["00000002","00000003"]});
 db.user.insert(document)
-document=({"user_id":"00000002", "user_name":"shiqiqi", "password":"123456", "email":"shiqiqi@gmail.com", "phone":"4121234567", "birth":new Date('May 16, 1991'),"trip_id":"0001"});
+document=({"user_id":"00000002", "user_name":"shiqiqi", "password":"123456", "email":"shiqiqi@gmail.com", "phone":"4121234567", "birth":new Date('May 16, 1991'),"trip_id":"0001","friend_id":["00000002","00000003"]});
 db.user.insert(document)
-document=({"user_id":"00000003", "user_name":"zhuangqiankun", "password":"123456", "email":"zhuangqiankun@gmail.com", "phone":"4125204002", "birth":new Date('May 16, 1991'),"trip_id":"0003"});
+document=({"user_id":"00000003", "user_name":"zhuangqiankun", "password":"123456", "email":"zhuangqiankun@gmail.com", "phone":"4125204002", "birth":new Date('May 16, 1991'),"trip_id":"0003","friend_id":null});
 db.user.insert(document)
-document=({"user_id":"00000004", "user_name":"chenglienanjie", "password":"123456", "email":"chenglienanjie@gmail.com", "phone":"4125204000", "birth":new Date('May 16, 1991'),"trip_id":"0004"});
+document=({"user_id":"00000004", "user_name":"chenglienanjie", "password":"123456", "email":"chenglienanjie@gmail.com", "phone":"4125204000", "birth":new Date('May 16, 1991'),"trip_id":"0004""friend_id":null});
 db.user.insert(document)
-document=({"user_id":"00000005", "user_name":"xiaolei", "password":"123456", "email":"xiaolei@gmail.com", "phone":"4125204000", "birth":new Date('May 16, 1991'),"trip_id":"0005"});
+document=({"user_id":"00000005", "user_name":"xiaolei", "password":"123456", "email":"xiaolei@gmail.com", "phone":"4125204000", "birth":new Date('May 16, 1991'),"trip_id":"0005","friend_id":null});
 db.user.insert(document)
-document=({"user_id":"00000006", "user_name":"zhanxiaoran", "password":"123456", "email":"zhanxiaoran@gmail.com", "phone":"4125204000", "birth":new Date('May 16, 1991'),"trip_id":"0006"});
+document=({"user_id":"00000006", "user_name":"zhanxiaoran", "password":"123456", "email":"zhanxiaoran@gmail.com", "phone":"4125204000", "birth":new Date('May 16, 1991'),"trip_id":"0006","friend_id":null});
 db.user.insert(document)
-document=({"user_id":"00000007", "user_name":"ranchenyang", "password":"123456", "email":"ranchenyang@gmail.com", "phone":"4125204000", "birth":new Date('May 16, 1991'),"trip_id":"0007"});
+document=({"user_id":"00000007", "user_name":"ranchenyang", "password":"123456", "email":"ranchenyang@gmail.com", "phone":"4125204000", "birth":new Date('May 16, 1991'),"trip_id":"0007","friend_id":null});
 db.user.insert(document)
-document=({"user_id":"00000008", "user_name":"liuzhiyue", "password":"123456", "email":"liuzhiyue@gmail.com", "phone":"4125204000", "birth":new Date('May 16, 1991'),"trip_id":"0008"});
+document=({"user_id":"00000008", "user_name":"liuzhiyue", "password":"123456", "email":"liuzhiyue@gmail.com", "phone":"4125204000", "birth":new Date('May 16, 1991'),"trip_id":"0008","friend_id":null});
 db.user.insert(document)
 document=({"trip_id":"0001","destination":"seattle","depart_date":new Date('May 10, 2014'),"return_date":new Date('May 16, 2014'),"a_id":["seattle_00000001","seattle_00000000"]});
 db.trip.insert(document)
